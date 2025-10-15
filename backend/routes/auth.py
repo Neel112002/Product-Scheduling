5868beb5574fdc6633955348207eb507769c4f2c
Neel shah <neelshah112002@gmail.com>
2025-10-15 14:40:23 -0400

    ForgetPassword
    Forgotpassword and change passwoord api created

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -2,9 +2,13 @@
 from flask import Blueprint
 from controllers.auth_controller import AuthController
 from flask_jwt_extended import jwt_required
+from controllers.password_flow_controller import PasswordFlowController
 
 router = Blueprint("auth", __name__, url_prefix="/auth")
 auth = AuthController()
+
+
+pwd = PasswordFlowController()
 
 # Wizard Registration (Owner -> Company -> Location)
 @router.post("/register")
@@ -27,3 +31,15 @@ def refresh():
 @jwt_required()
 def me():
     return auth.me()
+
+@router.post("/forgot-password")
+def forgot_password():
+    return pwd.forgot_password()
+
+@router.post("/forgot-password/confirm")
+def forgot_password_confirm():
+    return pwd.forgot_password_confirm()
+
+@router.post("/change-password")
+def change_password():
+    return pwd.change_password()
